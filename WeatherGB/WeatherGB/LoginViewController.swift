4721e4f87018d6bc8cd352a8a7d8560bd38f324f
Kestrel <zimaleksey@gmail.com>
2022-05-19 19:03:12 +0500

    Start again

---
+++ WeatherGB/WeatherGB/LoginViewController.swift
@@ -0,0 +1,58 @@
+//
+//  LoginViewController.swift
+//  WeatherGB
+//
+//  Created by Aleksy on 18.05.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func loginButton(_ sender: Any) {
+        guard let login = loginField.text,
+              let password = passwordField.text else {return}
+            
+        
+        if  login == "",
+            password == "" {
+            print("Good")
+              loginField.backgroundColor = .green
+                passwordField.backgroundColor = .green
+        } else {
+                loginField.backgroundColor = .red
+                    passwordField.backgroundColor = .red
+                    return
+                }
+    
+    }
+    
+    
+    
+    
+
+}
